59e7bc991bdd0aa3195e990a96258f900d7386bf
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-04-26 09:42:35 +0200

    Added unsplash api implementation

---
+++ client/src/api/unsplash.ts
@@ -0,0 +1,20 @@
+// ES Modules syntax
+import Unsplash, { toJson } from "unsplash-js";
+
+const unsplash: Unsplash = new Unsplash({
+	accessKey: process.env.REACT_APP_UNSPLASH_KEY || ""
+});
+
+type Search = (query: string) => Promise<string>;
+
+const search: Search = (query) =>
+	unsplash.search
+		.photos(query, 1, 1, { orientation: "landscape" })
+		.then(toJson)
+		.then((json) => {
+			console.log(json);
+			return json.results[0].urls.raw;
+		})
+		.catch(() => "");
+
+export default search;
