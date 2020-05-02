b76f3d24cf1a11d5c874b9ed73e0e277f131cd57
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-05-02 21:12:35 +0200

    removed dependency on client auth in backend

--- functions/functions/util/firebase.js
+++ functions/functions/util/firebase.js
@@ -1,17 +1,16 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
 const adminConfig = require("../admin.json");
-const client = require("firebase/app");
 require("firebase/auth");
 
 admin.initializeApp({
-	credential: admin.credential.cert(adminConfig),
-	databaseURL: "https://keeps-81a16.firebaseio.com"
+  credential: admin.credential.cert(adminConfig),
+  databaseURL: "https://keeps-81a16.firebaseio.com",
 });
 // admin.initializeApp(functions.config().firebase);
 
 module.exports = {
-	admin,
-	auth: admin.auth(),
-	db: admin.firestore()
+  admin,
+  auth: admin.auth(),
+  db: admin.firestore(),
 };
