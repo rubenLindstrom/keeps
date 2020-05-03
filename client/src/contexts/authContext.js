3b873aba4a78f3d6ea23b3f267af332bc323809c
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-05-03 12:35:30 +0200

    Updated server error message parsing

--- client/src/contexts/authContext.js
+++ client/src/contexts/authContext.js
@@ -2,11 +2,11 @@ import React, { useState, useEffect, createContext } from "react";
 import { useLocation } from "react-router-dom";
 import auth from "../firebaseAuth";
 import {
-	isEmpty,
-	isEmail,
-	translateClientError,
-	translateServerError,
-	hasErrors
+  isEmpty,
+  isEmail,
+  translateClientError,
+  translateServerError,
+  hasErrors,
 } from "../helpers";
 import { errorMessages, PASSWORD_MIN_LENGTH } from "../constants";
 
@@ -15,104 +15,102 @@ import { doRegister, setToken, unsetToken } from "../services";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-	const [authenticated, setAuthenticated] = useState(null);
-	const [user, setUser] = useState(null);
-	const [errors, setErrors] = useState({});
-	const [loading, setLoading] = useState(false);
-	const location = useLocation();
+  const [authenticated, setAuthenticated] = useState(null);
+  const [user, setUser] = useState(null);
+  const [errors, setErrors] = useState({});
+  const [loading, setLoading] = useState(false);
+  const location = useLocation();
 
-	useEffect(() => {
-		setErrors({});
-	}, [location.pathname]);
+  useEffect(() => {
+    setErrors({});
+  }, [location.pathname]);
 
-	useEffect(() => {
-		auth.onAuthStateChanged((user) => {
-			if (user) {
-				setUser({ email: user.email, uid: user.uid });
-				auth.currentUser
-					.getIdToken(true)
-					.then((token) => {
-						console.log(token);
-						return token;
-					})
-					.then((token) => setToken(token))
-					.then(() => setAuthenticated(true))
-					.catch(() => setAuthenticated(false));
-			} else {
-				setAuthenticated(false);
-				setUser(null);
-			}
-		});
-	}, []);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser({ email: user.email, uid: user.uid });
+        auth.currentUser
+          .getIdToken(true)
+          .then((token) => {
+            console.log(token);
+            return token;
+          })
+          .then((token) => setToken(token))
+          .then(() => setAuthenticated(true))
+          .catch(() => setAuthenticated(false));
+      } else {
+        setAuthenticated(false);
+        setUser(null);
+      }
+    });
+  }, []);
 
-	const login = (email, password) => {
-		const validation = {};
-		if (isEmpty(email)) validation.email = errorMessages.mustNotBeEmpty;
-		else if (!isEmail(email)) validation.email = errorMessages.invalidEmail;
-		if (isEmpty(password))
-			validation.password = errorMessages.mustNotBeEmpty;
-		else if (password.length < PASSWORD_MIN_LENGTH)
-			validation.password = errorMessages.tooShort;
+  const login = (email, password) => {
+    const validation = {};
+    if (isEmpty(email)) validation.email = errorMessages.mustNotBeEmpty;
+    else if (!isEmail(email)) validation.email = errorMessages.invalidEmail;
+    if (isEmpty(password)) validation.password = errorMessages.mustNotBeEmpty;
+    else if (password.length < PASSWORD_MIN_LENGTH)
+      validation.password = errorMessages.tooShort;
 
-		if (hasErrors(validation).length)
-			return setErrors(translateServerError(validation));
+    if (hasErrors(validation).length)
+      return setErrors(translateServerError(validation));
 
-		setErrors({});
-		setLoading(true);
-		auth.signInWithEmailAndPassword(email, password)
-			.then()
-			.catch((err) => {
-				setErrors(translateClientError(err));
-			})
-			.finally(() => setLoading(false));
-	};
+    setErrors({});
+    setLoading(true);
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then()
+      .catch((err) => {
+        setErrors(translateClientError(err));
+      })
+      .finally(() => setLoading(false));
+  };
 
-	const register = (email, password, cPassword) => {
-		const validation = {};
-		if (isEmpty(email)) validation.email = errorMessages.mustNotBeEmpty;
-		else if (!isEmail(email)) validation.email = errorMessages.invalidEmail;
+  const register = (email, password, cPassword) => {
+    const validation = {};
+    if (isEmpty(email)) validation.email = errorMessages.mustNotBeEmpty;
+    else if (!isEmail(email)) validation.email = errorMessages.invalidEmail;
 
-		if (isEmpty(password))
-			validation.password = errorMessages.mustNotBeEmpty;
-		else if (password.length < PASSWORD_MIN_LENGTH)
-			validation.password = errorMessages.tooShort;
+    if (isEmpty(password)) validation.password = errorMessages.mustNotBeEmpty;
+    else if (password.length < PASSWORD_MIN_LENGTH)
+      validation.password = errorMessages.tooShort;
 
-		if (isEmpty(cPassword))
-			validation.cPassword = errorMessages.mustNotBeEmpty;
-		else if (cPassword !== password)
-			validation.cPassword = errorMessages.mustMatch;
+    if (isEmpty(cPassword)) validation.cPassword = errorMessages.mustNotBeEmpty;
+    else if (cPassword !== password)
+      validation.cPassword = errorMessages.mustMatch;
 
-		if (hasErrors(validation).length)
-			return setErrors(translateServerError(validation));
+    if (hasErrors(validation).length)
+      return setErrors(translateServerError(validation));
 
-		setErrors({});
-		setLoading(true);
-		doRegister(email, password, cPassword)
-			.then(() => login(email, password))
-			.catch((err) => setErrors(translateServerError(err)))
-			.finally(() => setLoading(false));
-	};
+    setErrors({});
+    setLoading(true);
+    doRegister(email, password, cPassword)
+      .then(() => login(email, password))
+      .catch((err) => setErrors(translateServerError(err.response.data)))
+      .finally(() => setLoading(false));
+  };
 
-	const logout = () => {
-		auth.signOut();
-		unsetToken();
-	};
+  const logout = () => {
+    auth.signOut();
+    unsetToken();
+  };
 
-	return (
-		<AuthContext.Provider
-			value={{
-				authenticated,
-				user,
-				login,
-				logout,
-				register,
-				errors,
-				loading
-			}}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{
+        authenticated,
+        user,
+        login,
+        logout,
+        register,
+        errors,
+        loading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthContext;
