68dbc87bf7ed4fb9f7cc37039190a1df73c1bf26
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-05-08 14:15:32 +0200

    Point 15: Added logo instead of text-based site title

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,9 +1,17 @@
 import React from "react";
+import styled from "styled-components";
 import Controls from "./controls";
+import logo from "../../images/logo.png";
+
+const Logo = styled.img`
+  width: 185px;
+  height: auto;
+  padding: 0.5rem 0;
+`;
 
 const nav = () => (
   <div className="nav">
-    <h1>Keeps</h1>
+    <Logo src={logo} alt="Logo" />
     <div className="note-controls">
       <Controls />
     </div>
