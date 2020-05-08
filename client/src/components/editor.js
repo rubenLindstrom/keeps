f01b64893425f3253ad8db09595c075829727f93
Ruben Lindström <ruben.lindstrom@gmail.com>
2020-05-08 09:44:07 +0200

    Point 7: Give text editor focus upon switching note

--- client/src/components/editor.js
+++ client/src/components/editor.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import RichTextEditor from "react-rte";
 
 import NoteContext from "../contexts/noteContext";
@@ -6,53 +6,75 @@ import NoteContext from "../contexts/noteContext";
 import { Error } from "./atoms";
 
 const EditorContainer = () => {
-	const {
-		selectedNote,
-		error,
-		loading,
-		editorValue,
-		setEditorValue,
-		saveNote
-	} = useContext(NoteContext);
+  const {
+    selectedNote,
+    error,
+    loading,
+    editorValue,
+    setEditorValue,
+    saveNote,
+  } = useContext(NoteContext);
 
-	const handleKeyPress = (e) => {
-		if (e.keyCode === 83 && e.ctrlKey) {
-			e.preventDefault();
-			saveNote(selectedNote.id);
-		}
-	};
+  const inputRef = useRef();
 
-	return (
-		<div onKeyDown={handleKeyPress}>
-			<EditorView
-				error={error}
-				loading={loading}
-				noNotes={!Boolean(selectedNote)}
-				value={editorValue}
-				onChange={setEditorValue}
-			/>
-		</div>
-	);
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 83 && e.ctrlKey) {
+      e.preventDefault();
+      saveNote(selectedNote.id);
+    }
+  };
+
+  const focusEditor = () => {
+    inputRef.current && inputRef.current._focus();
+  };
+
+  useEffect(() => {
+    focusEditor();
+  }, [selectedNote]);
+
+  return (
+    <div onKeyDown={handleKeyPress}>
+      <EditorView
+        error={error}
+        loading={loading}
+        noNotes={!Boolean(selectedNote)}
+        value={editorValue}
+        onChange={setEditorValue}
+        inputRef={inputRef}
+        onClick={focusEditor}
+      />
+    </div>
+  );
 };
 
-const EditorView = ({ loading, error, value, onChange, noNotes }) => {
-	if (error || loading || noNotes) {
-		let content;
-		if (error) content = <Error>{error.error}</Error>;
-		// TODO: Insert spinner instead
-		else if (loading) content = <p className="big">Fetching notes...</p>;
-		else content = <p className="big">You have no notes :(</p>;
-		return <div style={{ padding: "1rem" }}>{content}</div>;
-	}
+const EditorView = ({
+  loading,
+  error,
+  value,
+  onChange,
+  noNotes,
+  inputRef,
+  onClick,
+}) => {
+  if (error || loading || noNotes) {
+    let content;
+    if (error) content = <Error>{error.error}</Error>;
+    // TODO: Insert spinner instead
+    else if (loading) content = <p className="big">Fetching notes...</p>;
+    else content = <p className="big">You have no notes :(</p>;
+    return <div style={{ padding: "1rem" }}>{content}</div>;
+  }
 
-	return (
-		<RichTextEditor
-			autoFocus
-			value={value}
-			onChange={onChange}
-			placeholder="Start editing your note here..."
-		/>
-	);
+  return (
+    <RichTextEditor
+      ref={inputRef}
+      autoFocus
+      value={value}
+      onChange={onChange}
+      placeholder="Start editing your note here..."
+      onClick={onClick}
+    />
+  );
 };
 
 export default EditorContainer;
